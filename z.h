61e5e84a6296a98c80cbffbedfc8b21b73a881ce
jack andrews <effbiae@gmail.com>
2025-03-22 04:00:02 +1100

    Fri 21 Mar 17:00:02 UTC 2025

--- z.h
+++ z.h
@@ -1,22 +1,25 @@
-#include"_.h"
-Z0*D=__DATE__"(c)arthur whitney (l)MIT",B=32,d[256];void*memset(void*d,int i,Un){VR=d;J(n,R_=(i0)i|z0)return d;}_D(i2,i$,i&&48>*s?-i$(i-1,s+1):r(0,i(i,r=10*r+si-48)),ii,ss)
-#if linux //m_(a?:A,x,3,i?0x12:O?0x1042:0x4022,i,0)
-#define O(f,i) AS(f,"mov %rcx,%r10;mov $"#i",%rax;syscall;",Ux,...) //scaling_cur_freq
-Z2 O;O(_k,60)O(_w,0)O(w_,1)O(_n,4)O(_d,3)O(d_,2)O(m_,9)O(_m,11)AS(_t,"rdtsc;shl $32,%rdx;or %rdx,%rax;")
-_U(nt,Z2 i;_t()*10./(i=i?:(_w(i=d_((U)"/sys/devices/system/cpu/cpu0/cpufreq/base_frequency",0),d,2),_d(i),i$(2,d))))
+#include"k.h"
+e2 t;Z0*D=__DATE__"(c)arthur whitney (l)MIT",B=32,d[256];_D(i2,i$,i&&48>*s?-i$(i-1,s+1):r(0,i(i,r=10*r+s[i]-48)),ii,ss)
+#if linux//x86
+#define O(f,i) AS(f,"mov %rcx,%r10;mov $"#i",%rax;syscall;",Ux,...)
+Z2 O;O(_k,60)O(_w,0)O(W_,1)O(_N,4)O(_d,3)O(d_,2)O(M_,9)O(_M,11)void _start(){asm("lea 8(%rsp),%rsi;call main;mov $60,%rax;syscall");}
+_U(t_,o(ldmxcsr)(0xbfc0);ii=d_((U)"/sys/devices/system/cpu/cpu0/cpufreq/base_frequency",0);_w(i,d,2);_d(i);1e8*i$(2,d))AS(_t,"rdtsc;shl $32,%rdx;or %rdx,%rax;")
 #else
 #define O(f,i) AS(f,"mov x16,"#i"\nldr x1,[sp]\nldr x2,[sp,8]\nldr x3,[sp,16]\nldr x4,[sp,24]\nmov x5,0\nsvc 0\n",Ux,...)
-Z2 O=1;O(_k,1)O(_w,3)O(w_,4)O(_n,338)O(_d,6)O(d_,5)O(m_,197)O(_m,73)AS(_t,"mrs x0,cntvct_el0\n")AS(t_,"mrs x0,cntfrq_el0\n")_U(nt,Z2 i;1e9/(i=i?:t_())*_t())
+Z2 O=1;O(_k,1)O(_w,3)O(W_,4)O(_N,338)O(_d,6)O(d_,5)O(M_,197)O(_M,73)AS(t_,"mrs x0,cntfrq_el0\n")AS(_t,"mrs x0,cntvct_el0\n")
 #endif
+#define Q(z) r(z,P(!r,0))
 #define ins(g,z) D(U,g,z,ii,in,ss)
-#define Us(g,z) _U(g,z,ss)
-Ze E9[]={1,10,100,1e3,1e4,1e5,1e6,1e7,1e8,1e9};U_(w2,w_(2,(U)s,i);0,ii,ss)U_(QQ,ws(s);_k(1),ss)_u(wl,i-=d[i-1]==B;P(192>i,w0(i,d))d[189]=d[190]=46;w2(191,d))f(wb,i(64,d[i]=48|1&x>>i)w0(64,d);x)
-_U(b0,a&=0x8080808080808080;Uu=0;i(8,u|=(U)((__uint128_t)0x204081020408100*a[i]>>64)%256<<8*i)u,j6 a)ins(nc,nu(b(i)&b0((i0)n==Vs)))UV(ib,iu(~b0(x)))is(id,ib((i0)i!=Vs))
-#define Q(z) r(z,P(-1==r,r))
-#define sx ((i0*)k_(2,x))
-extern U k_(i2,U),k(i2,U,U),K(U,U,U);f(_r,ax?x:k_(0,x))f(r_,ax?x:k_(1,x))
-Z0 F[27][64];ZU C[27][8],G[27],wx(U),ls(ss),qs(ii,ss),p(ii,ss),q(ii),e(i2,i0**),zs(ii,ss),ts(ii,ss);
-is(ns,R(1,i,*rV=Vs))_u(wvf,i=id(i,"wf");P(!i--,k_(0,0))ss=d;j(27,Ux=G[j];*s=96+j;s+=x&&i^px)ns(s-d,d))
-#define _s s[-1]
-#define xi x2[i]
-#define xe ei(xi)
+U_(w_,W_(2,s,n),in,ss)Ui(wc,w_(1,&i);0)is(wl,w_(i,s);wc(10))is(ic,sd=s;W(i!=*s)++s;s-d)_U(ws,wl(ic(0,s),s),ss)
+_U(_n,Ua[24];_N((U)s,a)|!(1&a[O?0:3]>>(O?47:15))?ws(s):a[O?12:6],ss)
+U_(m_,Q(*a=_n(s));ii=d_((U)s,0);r(M_(0,*a,3,2,i,0),_d(i)),U*a,ss)
+U_(_m,_M(a,x),Ua,Ux)
+void k_(U);U ku(i2,U);void exit(ii){_k(i);}typedef long(*_)(U);e2 E9[]={1,10,1e2,1e3,1e4,1e5,1e6,1e7,1e8,1e9};
+_U(b0,z&=0x8080808080808080;Ux=0;i(8,x|=(U)((__uint128_t)0x204081020408100*z[i]>>64)%256<<8*i)x,Jz)ins(nc,nu(b(i)&b0((i0)n==Vs)))
+#define sx ((i0*)ku(2,x))
+#define ux ku(3,x)
+#define H(g,z) _U(g,z,Uf,Ua,Ux)
+_f(_r,ax?x:ku(0,x))_f(r_,ax?x:ku(1,x))_g(_i,k(12,x,ti(i)))H(k3,0)
+
+#define T(g,z) _U(g,ss=*_;U r=_(z);*_=s;r,ii,i0**_)
+#define U(g,z) _U(g,ss=*_;U r=_(z);*_=s;r,ii,i0**_,Ux)

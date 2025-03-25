9dde980e8833a7184e7f33f2b8d77cc547d55654
jack andrews <effbiae@gmail.com>
2025-03-26 03:30:02 +1100

    Tue 25 Mar 16:30:02 UTC 2025

--- _.h
+++ _.h
@@ -1,19 +1,45 @@
-#define $b(g,i,j) (2<bx?_(j):bx?_(i):_(g))
-#define ox (b(17)&x>>38)
+#define mn(g,z) VD(g,z,ii,in,iR,iX)
 #define ux (mx?Ox>>47:nx)    
 #define sx ((i0*)(b(41)<<6&Ox))
-#include"k.h"
 #define I(n,z) i(n6(n),z)
+#define X(z) r(uz(x),GX=sx;GR=sr;I(Nr,R_=_(z)))
+#include"k.h" //typedef i0 X Vn(4),Y Vn(5),Z Vn(6);
+#define UV(g,z) U(g,z,Vx)
+#define VU(g,z) V(g,z,Ux)
+#define Vf(g,z) V(g,z,Vx)
+#define Vg(g,z) V(g,z,ii,Vx)
+#define VF(g,z) V(g,z,Va,Vx)
+#define VG(g,z) V(g,z,ii,Va,Vx)
+#define VH(g,z) V(g,z,Va,Vb,Vx)
+V(wI,i(16,wc(48|zi/10);wc(48|zi%10);wc(32))wc(10);z,Iz)
+#define o(o) B(ia32_##o)
+#define qx (1l<<36>(U)sx-(1l<<32))
+#define ox (b(17)&x>>38)
 #define Ox O[ox]
 #define bx b[tx]
 #define hx (31&x>>55)
+#define Nx (nx<<bx)
+#define ex (3>tx?(int)ix:ei(ix))
+#define eE(f,z) ((e6)f((z)-ze))[0]
+#define _R(t,n,z) _x(R(t,n,z))
+#define _X(t,z)   _x(R(tx,nx,t##R=sr;z))
+#define RU(n,z)   _x(R(0,n,UR=sr;i(nr,R_=_(z))))
+#define RV(t,n,z)    R(t,n,GR=sr;I(Nr,R_=_(z)))
+#define Ya i5 a
+#define Yb i5 b
 #define Yx i5 x
-#define RX ((g4*)sr)[i]
-#define RY ((i5*)sr)[i]
-#define RZ rV[i]
-#define XX ((g4*)sx)[i]
-#define XY ((i5*)sx)[i]
-#define XZ xV[i]
+#define Yy i5 y
+#define Xx g4 x
+#define X4 ((g4*)sx)[i]
+#define X5 ((i5*)sx)[i]
+#define X6 xZ[i]
+#define R4 ((g4*)sr)[i]
+#define R5 ((i5*)sr)[i]
+#define R6 rZ[i]
+#define A6 x(a,X6)
+#define Ix ((i6)X_)
+#define Jx ((j6)X_)
+#define Ex ((e6)X_)
 #define ai a[i]
 #define bi b[i]
 #define Ai A[i]
@@ -23,62 +49,21 @@
 #define Ri R[i]
 #define Rk R[k|i]
 #define zk z[k|i]
-#define Vx g6 x
-#define Iz i6 z
-#define IA i6*A
-#define VA g6*A
-#define VX g6*X
-#define A_ *A++
-#define X_ *X++
-#define Ib i6 b
-#define Z_(g,z) void g(){z;}
-#define UV(g,z) _U(g,z,Vx)
-#define Vf(g,z) _V(g,z,Vx)
-#define VF(g,z) _V(g,z,Va,Vx)
-#define VG(g,z) _V(g,z,ii,Va,Vx)
+#define VD(g,z,x...) static void g(x){z;}
+#define IF(g,z) D(i6,g,z,i6 a,i6 x)
 #define I2 ((i6){0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15})
-#define _R(t,n,z) _x(R(t,n,z))
-#define _X(t,z)   _x(R(tx,nx,t##R=sr;z))
-#define RU(n,z)   _x(R(0,n,UR=sr;i(nr,R_=_(z))))
-#define RV(t,n,z)    R(t,n,VR=sr;I(Nr,R_=_(z)))
-#define X(z) r(uz(x),VX=sx;VR=sr;I(Nr,R_=_(z)))
-#define V3(g,z) _V(g,z,Va,Vb,Vx)
-U m_(U*,ss),_m(U,U);
-ZI BA=I2+1-I2%2*2,AB=I2%2-1;
-ZJ I3={0,1,2,3,4,5,6,7};
-typedef i0 g4 V_(4);typedef i2 i5 V_(5);typedef e2 e5 V_(5);typedef int s6 V_(6);_Z(M6,i(n6(n),A_=X_),in,VA,VX)_D(j6,RJ,x<<i|x>>64-i,ii,j6 x)
-#define IF(g,z) _D(i6,g,z,i6 a,i6 x)
-#define C(t,z) bu(convertvector)(z,t)
-#define g(g,z) D(U,g,z,ii,Ux)
-#define F(g,z) D(U,g,z,Ua,Ux)
-#define G(g,z) D(U,g,z,ii,Ua,Ux)
-#define Nx (nx<<bx)
-#define ex (3>tx?(int)ix:ei(ix))
-#define hh i1 h
+U m_(U*,ss),_m(U,U);ZI AB=I2%2-1,BA=I2+1-I2%2*2;
+ZJ I3={0,1,2,3,4,5,6,7};VD(M6,i(n6(n),A_=X_),in,GA,GX)D(j6,RJ,z<<i|z>>64-i,ii,Jz)
+#define C(t,z) B(convertvector)(z,t)
 #define IB i6*B
 #define Ia (i6)A_
+#define Ja (j6)A_
 #define Ea (e6)A_
 #define Ud *(U*)d
 #define Us *(U*)s
-#define ih i2 h
-#define ik i2 k
-#define il i2 l
-#define im i2 m
-#define it i2 t
 #define Vz g6 z
 #define UX U*X
 #define UR U*R
-#define sa x(a,sx)
-#define ta x(a,tx)
-#define ma x(a,mx)
-#define na x(a,nx)
-#define nr x(r,nx)
-#define Zr x(r,Zx)
-#define Za x(a,Zx)
-#define ka x(a,kx)
-#define ia x(a,ix)
-#define ea x(a,ex)
-#define ga (i0)ia
 #define iR i2*R
 #define iA i2*A
 #define iB i2*B
@@ -93,42 +78,34 @@ typedef i0 g4 V_(4);typedef i2 i5 V_(5);typedef e2 e5 V_(5);typedef int s6 V_(6)
 #define ER e6*R
 #define EA e6*A
 #define EX e6*X
-#define Eb e6 b
-#define Ec e6 c
-#define Ib i6 b
-#define Ic i6 c
-#define Ez e6 z
-#define Gz g6 z
-#define Sz s6 z
-#define Ix ((i6)X_)
-#define Jx ((j6)X_)
-#define Ex ((e6)X_)
+#define sa x(a,sx)
+#define ta x(a,tx)
+#define ma x(a,mx)
+#define na x(a,nx)
+#define nr x(r,nx)
+#define ka x(a,kx)
+#define ia x(a,ix)
+#define ea x(a,ex)
 #define Nr x(r,Nx)
 #define va x(a,vx)
 #define Na x(a,Nx)
-#define xE ((e6*)sx)
-#define aV x(a,xV)
-#define OO(b) if(b)exit(1);
-#define VU(g,z) _V(g,z,Ux)
-#define Vg(g,z) _V(g,z,ii,Vx)
-#define Ef(g,z) _D(e6,g,z,e6 x)
-#define EF(g,z) _D(e6,g,z,e6 a,e6 x)
-#define f_(g,z) U g(Ux){return({z;});}
-#define g_(g,z) U g(ii,Ux){return({z;});}
-#define G_(g,z) U g(ii,Ua,Ux){return({z;});}
 #define Qi(b,i) if(b)return i;
 #define Qz(b) Qi(b,0)
 #define Qr(b) Qi(b,1)
 #define Qn(b) Qi(b,2)
 #define Qt(b) Qi(b,3)
-#define n4(z) (15+(z)>>4)
-#define $6(z,a,b,c,d,e,f) ({i2 $=z;!$?_(a):1==$?_(b):2==$?_(c):3==$?_(d):4==$?_(e):_(f);})
-#define $7(z,a,b,c,d,e,f,g) ({i2 $=z;!$?_(a):1==$?_(b):2==$?_(c):3==$?_(d):4==$?_(e):5==$?_(f):_(g);})
+#define Ef(g,z) D(e6,g,z,e6 x)
+#define EF(g,z) D(e6,g,z,e6 a,e6 x)
+#define f_(g,z) U g(Ux){return({z;});}
+#define g_(g,z) U g(ii,Ux){return({z;});}
+#define G_(g,z) U g(ii,Ua,Ux){return({z;});}
+#define inx(g,z) U(g,z,ii,in,Ux)
 #define $a(z,a,b,c,d,e,f,g,h,i,j) ({i2 $=z;!$?_(a):1==$?_(b):2==$?_(c):3==$?_(d):4==$?_(e):5==$?_(f):6==$?_(g):7==$?_(h):8==$?_(i):_(j);})
-#define p6(a,b,c,d,e,f) ((e2)a+x*((e2)b+x*((e2)c+x*((e2)d+x*((e2)e+x*(e2)f)))))
-#define inx(g,z) _U(g,z,ii,in,Ux)
-#if 14>__clang_major__
-VF(LG,bu(ia32_pminub512)(a,x))VF(LI,bu(ia32_pminud512)(a,x))VF(LE,bu(ia32_minps512)(a,x,4))VF(MG,bu(ia32_pmaxub512)(a,x))VF(MI,bu(ia32_pmaxud512)(a,x))VF(ME,bu(ia32_maxps512)(a,x,4))
-#else
-VF(LG,bu(elementwise_min)(a,x))VF(MG,bu(elementwise_max)(a,x))IF(LI,bu(elementwise_min)(a,x))IF(MI,bu(elementwise_max)(a,x))EF(LE,bu(elementwise_min)(a,x))EF(ME,bu(elementwise_max)(a,x))
-#endif
+#define g0 g[0]
+#define g1 g[1]
+#define g2 g[2]
+#define g3 g[3]
+#define h0 h[0]
+#define h1 h[1]
+#define h2 h[2]
+#define h3 h[3]
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
-o=-Ofast -fno-builtin -funsigned-char -fno-unwind-tables -Wno-multichar -Wno-parentheses -Wno-incompatible-pointer-types -Wfatal-errors
+o=-Ofast -fno-builtin -funsigned-char -fno-unwind-tables -Wno-multichar -Wno-parentheses -Wno-incompatible-pointer-types -Wfatal-errors -Wno-psabi
 
 a:?.[ch] makefile
-	clang $o -oa ?.c -s -nostdlib -mavx512f -mavx512dq -mavx512vbmi
+	clang $o -oa ?.c -s -nostdlib -mavx2
 

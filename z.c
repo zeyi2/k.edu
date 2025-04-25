08e472123e173310e16e4a0fca1699033e2021c6
jack andrews <effbiae@gmail.com>
2025-04-25 23:00:02 +1000

    Fri 25 Apr 13:00:02 UTC 2025

--- z.c
+++ z.c
@@ -1,22 +1,21 @@
-#include"z.h"//eval v+:(i=v(*s)?*s--:0;ik=3==s[-1]?*--s%B:0;U*_=G+*s--%B;r_(*_=i?y(i%B,*_,x):(_r(*_),x))) 
+#include"z.b"//eval v+:(i=v(*s)?*s--:0;ik=3==s[-1]?*--s%B:0;U*_=G+*s--%B;r_(*_=i?y(i%B,*_,x):(_r(*_),x)))  
 ss;
-Z0*P=":+-*%&|<>=~!@?#_^,.$RENLMS___'/\\()",*E=" nyi rnk len typ",F[27][256];ZU*A,C[27][32],G[26],e(U),w(ii,ss),p(ii,ss);g(S,_r(Gi);Gi=x;B)Ui(v,33>i-B)Ui(c,A=Ci;s=Fi+*Fi;e(0))
-g(fx,U*a=A;sd=s;Ub=G[23];r(c(i),S(23,b);s=d;A=a))H(z,OO(aa)Ub[na];i(na,bi=Gi;Gi=ta?k(12,r_(a),ti(i)):aj)x=fx(f,x);i(na,Gi=bi)x)H(Z,OO(ta)i(na,Q(x=z(f,aj,x)))x)
-H(y,12==f&&9>a-1?fx(a,x):4>(x=pf?k(f,a,x):0)?(E[x*=4]=P[f],wl(4,E+x)):x)U(n,ii=*s--;65==i?e(0):r_(B>i?Ai:G[i-97]?:wl(1,&i)))U(N,Ux=Q(n());W(!v(*s))x=Q(y(12,Q(n()),x));x)
-f(e,x=x?:Q(N());ii=*s---B;B>i?e(Q(3>i-29>v(*s)?_(Uf=Q(n());OO(B<=f)Ua=Q(N());_a((30>i?z:Z)(f,a,x))):i?y(i,v(*s)?29>i?0:*s--:Q(N()),x):_(S(*s-97,x);64>*--s?r_(x):B))):x)
+Z0*P=":+-*%&|<>=~!@?#_^,.$RENLMS___'/\\()",*Y=" nyi rnk len typ",F[27][256];ZU*A,C[27][32],G[26],a(U),w(ii,ss),p(ii,ss);g(S,_r(Gi);Gi=x;B)Ui(v,33>i-B)Ui(c,A=Ci;s=Fi+*Fi;a(0))
+g(fx,U*a=A;sd=s;Ub=G[23];r(c(i),S(23,b);s=d;A=a))H(z,Q(aa)Ub[na];i(na,bi=Gi;Gi=ta?k(12,r_(a),ti(i)):aj)x=fx(f,x);i(na,Gi=bi)x)H(Z,Q(ta)i(na,Y(x=z(f,aj,x)))x)
+H(y,12==f&&9>a-1?fx(a,x):4>(x=pf?k(f,a,x):0)?(Y[x*=4]=P[f],wl(4,Y+x)):x)U(n,ii=*s--;65==i?a(0):r_(B>i?Ai:G[i-97]?:wl(1,&i)))U(N,Ux=Y(n());W(!v(*s))x=Y(y(12,Y(n()),x));x)
+f(a,x=x?:Y(N());ii=*s---B;B>i?a(Y(3>i-29>v(*s)?_(Uf=Y(n());Q(B<=f)Ua=Y(N());_a((30>i?z:Z)(f,a,x))):i?y(i,v(*s)?29>i?0:*s--:Y(N()),x):_(S(*s-97,x);64>*--s?r_(x):B))):x)
+e(e$,e2 strtof();strtof(s,0),in,ss)U($e,i2 sprintf();sprintf(s,"%.4g",e),ee,ss)
 //parse !"#$%&'()*+, -./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ 23+7+52+10+"`  0a( "23+sum qw" (0+Sa;23;qwe)
 Ui(P_,",+ .3$%=@A#!1\"2>& *         ')(-,____5______786___49_______B?C0/"[64>i?i:i-76])Ui(_p,i(32,_r(Ai);Ai=0)0)Ui($p,ss=Fi+2;r(*Fi-1,i(r,d[i]=B>si?48+si:96>si?P[si-B]:si)))
-D(e2,e$,ij;46>*s?-e$(i-1,s+1):i>(j=ib(101,s))?E9[15&s[j+1]]*e$(j,s):i>(j=ib(46,s))?(i-=j+1,(E9[i]*i$(j,s)+i$(i,s+j+1))/E9[i]):i$(i,s),ii,ss)is(n$,(8>i?b(8*i):-1)&*(U*)s)
 ins(vs,it=i;cc=7-t?B:96;sd=s+n;im=nc(n,c,s);m++?x(k_(t,m),i(m,n=l(d-s,ib(c,s));$3(t-2>>1,xi=i$(n,s),xe=e$(n,s),xj=n$(n,s));s+=n+1)x):$3(t-2>>1,ti(i$(n,s)),te(e$(n,s)),t(7,n$(n,s))))
-ins(js,Va=Vs;Vx=101==a|46==a;VR=s-1;Vb=x|10>a-48;r(iu(~b0(1>I0|$4(n,34!=a,b,b|32==a|1>a-45&32==*R,27>a-96|b)))+2*!n,ij=7==n;Ai=$3(n,ns(r-2,s),gs(r,s),vs(j?n:r>iu(b0(x))?5:2,r-j,s+j))))
+ins(js,Va=Vs;Vx=101==a|46==a;g6*R=s-1;Vb=x|10>a-48;r(iu(~b0(1>I0|$4(n,34!=a,b,b|32==a|1>a-45&32==*R,27>a-96|b)))+2*!n,ij=7==n;Ai=$3(n,ns(r-2,s),gs(r,s),vs(j?n:r>iu(b0(x))?5:2,r-j,s+j))))
 is(p,A=Ci;in=0;sd=Fi;*++d=64;ij;W(j=*s;B<j&59!=j)s+=8>(j=34==j?0:48==j&10>s[1]-48u?1:10>s[1>j-45&v(*d)]-48u?2:96==j?7:26>j-97?j:P_(j-B))?js(*++d=n++,j,s+!j):95!=(*++d=j);*Fi=d-Fi;i)
-is(q,si=0;L(59,'::'==*(h1*)(s+1)?S(*s-97,p(*s%B,s+3)):r(c(p(0,s)),_p(0)))x)Us(ls,ii;sd=s=(g0*)Q(m_(&i,s));L(10,47-*s?w(i,s):B)_m(i,d);B)Ui(ts,Ua=T()+14*i;i(i,Q(_r(c(0))))ti((T()-a)/1e6))
-//print f:ee:+[99|99.99|9.9e9]
-Ui(wd,P(192>i,wl(i,d))w_(189,d);ws(".."))U($n,*R=*(U*)&x;7+lu(x+1)>>3,Ux,U*R)ins($l,r(i,W(i--)(si=48|n%10,n/=10)))is($i,i>>31?(*s++=45,1+$i(-i,s)):$l(1+L_(i),i,s))D(e2,ei,*(e2*)&i,ii)
-U($e,*s++="+-"[0>e];e=0>e?-e:e;P(l,1+$l(2,.5+100*e,s))in=.5+1e2*e;P(1e4>n,$l(2,n/100,s);s[2]=46;3+$l(2,n%100,s+3))n=10*e/E9[L_(e)];i(5,si=48+$5(i,n/10,-2,n%10,53,L_(e)))6,il,ee,ss)
-U($a,$4(i-5,$e(l,ex,s),2*$e(l,ei(x>>32),s+$e(l,ex,s)),$n(x,s),l?$l(l,x,s):$i(x,s)),il,ss,ii,Ux)f(lz,il=r(0,i(nx,r=m(r,5>tx?xi:b(31)&xi)));l>E1?0:5>tx?1+L_(l):1+r(0,i(nx,r|=xi&&E1!=xi)))
+#define X(c,z) Ux;{sd=s+i;ii=-1;W((s+=i+1)<d&&(i=(10-c?ib:ic)(c,s)))Y(x=_(z));}
+is(q,si=0;X(59,'::'==*(i1*)(s+1)?S(*s-97,p(*s%B,s+3)):r(c(p(0,s)),_p(0)))x)Us(ls,ii;sd=s=(i0*)Y(m_(&i,s));X(10,47-*s?w(i,s):B)_m(i,d);B)Ui(ts,Ua=t*T()+14*i;i(i,Y(_r(c(0))))ti((t*T()-a)/1e6))
+//print
+U($a,$4(i-5,$e(ex,s),2*$e(ei(x>>32),s+$e(ex,s)),$n(x,s),l?$l(l,x,s):$i(x,s)),il,ss,ii,Ux)f(lz,im=r(0,i(nx,r=m(r,3>tx?xi:b(31)&xi)));E1<m?0:1+L(m))
 g(wm,il=i;im=mx?ux:1;h(l(16,m),ss=d;ii=h*vx;ij=i+vx;do $(1-l,s+=$a(mx|3>l?l:0,s,tx,6>tx?xi:xj);*s++=B)*s++=48|(2>tx?xg:3>tx?xi:!!xi);W(++i<j&192>s-d);wd(s-d))if(16<m)ws("..");B)
-U_(wx,px?x%B?B>x?wd($p(x)):wl(1,P+x-B):0:ax?wd($a(0,d,tx,jx)):_x(!nx?0:1==tx&B<=*sx?wl(nx,sx):tx?wm(2>tx?1:lz(x),x):ws("lst"));x,Ux)
+U_(wx,px?x%B?B>x?wd($p(x)):wl(1,P+x-B):0:ax?wd($a(0,d,tx,jx)):_x(!nx?0:1==tx&B<=*sx?wl(nx,sx):tx?wm(2>tx?1:3>tx?lz(x):0,x):ws("lst"));x,Ux)
 //repl
 is(u,si=0;$4(ib(*++s,"\\lt"),_k(0),ls(s+2),i=ic(B,++s);in=i?iz(q(i,s)):1;p(0,s+i+1);r(ts(n),_p(0)),P('w'==*s,k_(0,1))s=d;i(26,*s=97+i;s+=!!Gi)ns(s-d,d)))is(w,wx((92-*s?q:u)(i,s)))
-int main(int i,g0**_){t=t_();M_(1L<<32,1l<<36,3,O,0,0);k_(0,0);if(s)w(ic(0,s),s);$(s=*++_,ls(s))ws(__DATE__"(c)arthur whitney (l)MIT");W(i=_w(wc(B),d,256))if(--i)w(i,d);}
+int main(int i,i0**_){_t();M_(1l<<32,1l<<36,3,O,0,0);k_(0,0);_t();if(s)w(ic(0,s),s);$(s=*++_,ls(s))ws(__DATE__"(c)arthur whitney (l)MIT");W(i=_w(wc(B),d,256))if(--i)w(i,d);}
